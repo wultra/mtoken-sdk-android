653c7c130936ec9430b9a01800029a62fcb4a67b
Jan Kobersky <kober32@gmail.com>
2023-11-23 16:33:39 +0100

    Added snapshot networking dependency

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -81,10 +81,10 @@ dependencies {
     // DO NOT UPGRADE ABOVE 3.12.X! Version 3.12 is the last version supporting TLS 1 and 1.1
     // If upgraded, the app will crash on android 4.4
     implementation("com.squareup.okhttp3:okhttp:3.12.13")
-    implementation("com.wultra.android.powerauth:powerauth-networking:1.2.2")
+    implementation("com.wultra.android.powerauth:powerauth-networking:1.3.0-SNAPSHOT")
 
     // Dependencies
-    compileOnly("com.wultra.android.powerauth:powerauth-sdk:1.7.8")
+    compileOnly("com.wultra.android.powerauth:powerauth-sdk:1.8.0")
     compileOnly("io.getlime.core:rest-model-base:1.2.0")
 
     // TestDependencies
@@ -92,7 +92,7 @@ dependencies {
 
     // Android tests
     androidTestImplementation("com.jakewharton.threetenabp:threetenabp:1.1.1")
-    androidTestImplementation("com.wultra.android.powerauth:powerauth-sdk:1.7.8")
+    androidTestImplementation("com.wultra.android.powerauth:powerauth-sdk:1.8.0")
     androidTestImplementation("com.wultra.android.powerauth:powerauth-networking:1.2.2")
     androidTestImplementation("androidx.test:runner:1.5.2")
     androidTestImplementation("junit:junit:4.13.2")
