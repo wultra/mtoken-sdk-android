9b7264e1b29ed9d2a699dd9975969985bdb2cb75
Marek Stransky <stranskymarek90@gmail.com>
2023-11-28 17:32:21 +0100

    #107 - change `TOTPUtils` to `PACUtils` and update format to handle JWTs but also deeplinks

--- library/src/main/java/com/wultra/android/mtokensdk/operation/PACUtils.kt
+++ library/src/main/java/com/wultra/android/mtokensdk/operation/PACUtils.kt
@@ -23,48 +23,58 @@ import com.google.gson.annotations.SerializedName
 import com.wultra.android.mtokensdk.common.Logger
 
 /**
- * Utility class used for handling TOTP
+ * Utility class used for handling Proximity Anti-fraud Checks
  */
-class TOTPUtils {
+class PACUtils {
 
-    /** Data payload which is returned from JWT parser */
-    data class OperationTOTPData(
+    /** Data payload which is returned from the parser */
+    data class PACData(
 
-        /** The ID of the operations associated with the TOTP */
-        @SerializedName("potp")
-        val totp: String,
+        /** The ID of the operation associated with the TOTP */
+        @SerializedName("oid")
+        val operationId: String,
 
         /** The actual Time-based one time password */
-        @SerializedName("oid")
-        val operationId: String
+        @SerializedName("potp")
+        val totp: String?
     )
 
     companion object {
 
         /** Method accepts deeplink Uri and returns payload data or null */
-        fun parseDeeplink(uri: Uri?): OperationTOTPData? {
+        fun parseDeeplink(uri: Uri?): PACData? {
             val query = uri?.query ?: return null
             val queryItems = query.split("&").associate {
                 val (key, value) = it.split("=")
                 key to value
             }
 
-            queryItems["code"]?.let {
-                return parseJWT(it)
+            // Deeplink can have two query items with operationId & optional totp or single query item with JWT value
+            queryItems["oid"]?.let { operationId ->
+                val totp = queryItems["potp"]
+                return PACData(operationId, totp)
+            } ?: queryItems.entries.firstOrNull()?.let {
+                return parseJWT(it.value)
             } ?: run {
-                Logger.e("Failed to parse deeplink. Key `code` not found")
+                Logger.e("Failed to parse deeplink. Valid keys not found in Uri: $uri")
+                return null
             }
-
-            Logger.e("Failed to parse deeplink from $uri")
-            return null
         }
 
-        /** Method accepts scanned code as a String and returns payload data or null */
-        fun parseQRCode(code: String): OperationTOTPData? {
-            return parseJWT(code)
+        /** Method accepts scanned code as a String and returns PAC data */
+        fun parseQRCode(code: String): PACData? {
+            val encodedURLString = Uri.encode(code)
+            val uri = Uri.parse(encodedURLString)
+
+            // if the QR code is in the deeplink format parse it the same way as the deeplink
+            return if (uri.scheme != null) {
+                parseDeeplink(uri)
+            } else {
+                parseJWT(code)
+            }
         }
 
-        private fun parseJWT(code: String): OperationTOTPData? {
+        private fun parseJWT(code: String): PACData? {
             val jwtParts = code.split(".")
             if (jwtParts.size > 1) {
                 // At this moment we don't care about header, we want only payload which is the second part of JWT
@@ -74,7 +84,7 @@ class TOTPUtils {
                     return try {
                         val dataPayload = Base64.decode(base64EncodedData, Base64.DEFAULT)
                         val json = String(dataPayload, Charsets.UTF_8)
-                        Gson().fromJson(json, OperationTOTPData::class.java)
+                        Gson().fromJson(json, PACData::class.java)
                     } catch (e: Exception) {
                         Logger.e("Failed to decode QR JWT from: $code")
                         Logger.e("With error: ${e.message}")
