946a08488bb40aae7d03758143dd9566a7527aa8
Marek Chrenko <marek.chrenko@wultra.com>
2025-08-01 12:56:21 +0200

    add Alert attribute type

--- library/src/main/java/com/wultra/android/mtokensdk/api/operation/AttributeTypeAdapter.kt
+++ library/src/main/java/com/wultra/android/mtokensdk/api/operation/AttributeTypeAdapter.kt
@@ -116,6 +116,7 @@ internal class AttributeTypeAdapter : TypeAdapter<Attribute>() {
                     label
                 )
                 Attribute.Type.IMAGE -> ImageAttribute(attr("thumbnailUrl") ?: return null, attr("originalUrl"), label)
+                Attribute.Type.ALERT -> AlertAttribute(attr("alertType") ?: return null, attr("title"), attr("message") ?: return null, label)
                 Attribute.Type.UNKNOWN -> Attribute(Attribute.Type.UNKNOWN, label)
             }
         }
--- library/src/main/java/com/wultra/android/mtokensdk/api/operation/model/OperationAttribute.kt
+++ library/src/main/java/com/wultra/android/mtokensdk/api/operation/model/OperationAttribute.kt
@@ -39,6 +39,7 @@ open class Attribute(
         PARTY_INFO,
         AMOUNT_CONVERSION,
         IMAGE,
+        ALERT,
         UNKNOWN
     }
 
@@ -199,3 +200,24 @@ class ImageAttribute(
     /** Image label */
     label: Label
 ): Attribute(Type.IMAGE, label)
+
+/**
+ * Alert attribute represents a notification or message that should be displayed to the user.
+ * It contains information about the type, title, and message of the alert.
+ */
+class AlertAttribute(
+    /**
+     * Type of the alert that determines its visual appearance.
+     * Possible values: SUCCESS, WARNING, INFO, ERROR
+     */
+    val alertType: String,
+    
+    /** Title of the alert. Optional. */
+    val title: String?,
+    
+    /** Message content of the alert */
+    val message: String,
+    
+    /** Label for the alert */
+    label: Label
+): Attribute(Type.ALERT, label)
