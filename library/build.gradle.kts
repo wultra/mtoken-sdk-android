b63a806adf5ef403b64aa4f7bcdeabc571a02541
Marek Stransky <stranskymarek90@gmail.com>
2025-08-12 16:10:11 +0200

    Revert "Update project"
    This reverts commit 1bd159afcc35fd51eb0ec020fe10280cc4523988.
    
    # Conflicts:
    #	library/build.gradle.kts

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -78,27 +78,27 @@ android {
 dependencies {
     // Bundled
     implementation("org.jetbrains.kotlin:kotlin-stdlib:${Constants.BuildScript.kotlinVersion}")
-    implementation("androidx.annotation:annotation:1.9.1")
-    implementation("com.google.code.gson:gson:2.13.1")
-    implementation("com.squareup.okhttp3:okhttp:5.1.0")
+    implementation("androidx.annotation:annotation:1.8.2")
+    implementation("com.google.code.gson:gson:2.11.0")
     implementation("com.jakewharton.threetenabp:threetenabp:1.1.1")
+    implementation("com.squareup.okhttp3:okhttp:4.9.3")
     implementation("com.wultra.android.powerauth:powerauth-networking:1.5.0")
 
     // Dependencies
-    compileOnly("io.getlime.core:rest-model-base:1.12.0")
     compileOnly("com.wultra.android.powerauth:powerauth-sdk:1.9.2")
+    compileOnly("io.getlime.core:rest-model-base:1.9.0")
 
     // TestDependencies
     testImplementation("junit:junit:4.13.2")
     testImplementation("com.jakewharton.threetenabp:threetenabp:1.1.1")
 
     // Android tests
-    androidTestImplementation("com.wultra.android.powerauth:powerauth-sdk:1.9.5")
     androidTestImplementation("com.jakewharton.threetenabp:threetenabp:1.1.1")
+    androidTestImplementation("com.wultra.android.powerauth:powerauth-sdk:1.9.2")
     androidTestImplementation("com.wultra.android.powerauth:powerauth-networking:1.5.0")
-    androidTestImplementation("androidx.test:runner:1.7.0")
+    androidTestImplementation("androidx.test:runner:1.6.2")
     androidTestImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test:core:1.7.0")
+    androidTestImplementation("androidx.test:core:1.6.1")
     androidTestImplementation(platform("org.jetbrains.kotlin:kotlin-bom:${Constants.BuildScript.kotlinVersion}"))
 }
 
