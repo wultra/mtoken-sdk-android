379a3d7c3ea3cba01ad312f807b5ebf05f021adc
Marek Chrenko <marek.chrenko@wultra.com>
2026-05-19 15:02:09 +0200

    cleanup: optimize imports

--- library/src/main/java/com/wultra/android/mtokensdk/api/operation/model/QROperation.kt
+++ library/src/main/java/com/wultra/android/mtokensdk/api/operation/model/QROperation.kt
@@ -19,7 +19,7 @@ package com.wultra.android.mtokensdk.api.operation.model
 import io.getlime.security.powerauth.core.CoreSignatureKeyId
 import io.getlime.security.powerauth.sdk.PowerAuthSDK
 import java.math.BigDecimal
-import java.util.*
+import java.util.Date
 
 /**
  * QR operation model class.
